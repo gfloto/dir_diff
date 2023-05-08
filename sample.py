f3a93d0ddfc91ac421b892c107bfcd6fa8358dd9
griffin_floto <gfloto@uoguelph.ca>
2023-05-08 09:03:00 -0400

    cat prep for cluster

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ import sys, os
 import torch
 import numpy as np
 from tqdm import tqdm
-from einops import repeat
+from einops import repeat, rearrange
 
 from model import Unet
 from process import sig, Process 
@@ -39,7 +39,10 @@ class Sampler:
     # get intial distribution at t = t_max
     def init_sample(self):
         var = 1/(2*self.theta)
-        sample = (np.sqrt(var) * torch.randn(self.batch_size, self.k-1, *self.end_shape)).to(self.device)
+        if args.dataset == 'mnist':
+            sample = (np.sqrt(var) * torch.randn(self.batch_size, self.k-1, *self.end_shape)).to(self.device)
+        elif args.dataset == 'cifar10':
+            sample = (np.sqrt(var) * torch.randn(self.batch_size, self.k-1, 3, *self.end_shape)).to(self.device)
         x = sig(sample) 
         return x
 
@@ -48,19 +51,27 @@ class Sampler:
     def update(self, model, x, t, dt, g_scale=1):
         # get f, g, g^2 score and dB
         f = self.process.sde_f(x)
-        g = self.process.sde_g(x)
-        g2_score = model(x, t)
+        g = self.process.sde_g(x)     
+
+        # if color image: [b, k, c, h, w] -> [b, k*c, h, w]
+        if len(x.shape) == 5: # reshape to fit Unet
+            x_ = rearrange(x, 'b k c ... -> b (k c) ...')
+
+        g2_score = model(x_, t)
+
+        if len(x.shape) == 5:
+            g2_score = rearrange(g2_score, 'b (k c) ... -> b k c ...', c=3)
 
         # check f is not nan
         assert torch.isnan(f).sum() == 0, f'f is nan: {f}'
-        dB = (np.sqrt(dt) * torch.randn_like(x)).to(self.device) 
+        dB = (np.sqrt(dt) * torch.randn_like(g2_score)).to(self.device) 
 
         # solve sde: https://en.wikipedia.org/wiki/Euler%E2%80%93Maruyama_method 
         gdB = torch.einsum('b i j ..., b j ... -> b i ...', g, dB)
         return (-f + g2_score)*dt + g_scale*gdB
 
     @torch.no_grad()
-    def __call__(self, model, T, save_path='sample.png', order=1, d=10, pad=1e-3):
+    def __call__(self, model, T, save_path='sample.png', order=1, d=10, pad=1e-3, vis=True):
         if save_path is not None:
             os.makedirs('imgs', exist_ok=True)
 
@@ -78,6 +89,7 @@ class Sampler:
         g_scale = 1.75*np.power(g_scale, 1.5)
 
         # sample loop
+        d = 20
         for i in tqdm(range(T)):
             # update x
             change = self.update(model, x, t, dt, g_scale=g_scale[i])
@@ -93,15 +105,18 @@ class Sampler:
             if save_path is not None:
                 # normalize change to be [0, 1] to visualize 
                 change = (change - change.min()) / (change.max() - change.min())
-                save_vis([x.clone(), change], f'imgs/{int(i/d)}.png', k=self.k)
+                if vis:
+                    save_vis([x.clone()], f'imgs/{int(i/d)}.png', k=self.k)
 
         # discretize
-        for i in range(int(T/d), int(T/d)+10):
-            save_vis(x, f'imgs/{i}.png', k=self.k)
+        if vis:
+            for i in range(int(T/d), int(T/d)+10):
+                save_vis(x, f'imgs/{i}.png', k=self.k)
 
         # save gif
-        if save_path is not None:
-            make_gif('imgs', save_path, int(T/d)+10)
+        if vis:
+            if save_path is not None:
+                make_gif('imgs', save_path, int(T/d)+10)
 
 import json
 import argparse
@@ -116,6 +131,7 @@ def get_sample_args():
     return args
 
 if __name__ == '__main__':
+    batch_size = 128
     sample_args = get_sample_args()
     path = os.path.join('results', sample_args.exp)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -125,12 +141,18 @@ if __name__ == '__main__':
     args = argparse.Namespace(**args)
 
     # load model
-    model = Unet(dim=64, channels=args.k-1).to(device)
+    if args.dataset == 'mnist':
+        ch = args.k if args.proc_type == 'cat' else args.k-1
+        model = Unet(dim=64, channels=ch).to(args.device)
+    elif args.dataset == 'cifar10':
+        ch = 3*args.k if args.proc_type == 'cat' else 3*(args.k-1)
+        model = Unet(dim=64, channels=ch).to(args.device)
+
     model_path = os.path.join('results', sample_args.exp, f'model.pt')
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval() 
     print(f'Loaded model from {model_path}')
 
     # sample from model
-    sampler = Sampler(args, batch_size=8, device=device)
-    sampler(model, T=1000, save_path=save_path(args, 'sample.gif'))
+    sampler = Sampler(args, batch_size=batch_size, device=device)
+    sampler(model, T=2000, save_path=save_path(args, 'sample.gif'))
